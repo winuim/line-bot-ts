58a9383088fae7da43e513c06cc1077382cfcb2e
Uema Yohei <winuim@gmail.com>
2021-04-04 22:56:23 +0900

    add fitbit api

--- src/controllers/fitbit.ts
+++ src/controllers/fitbit.ts
@@ -1,110 +1,13 @@
 import {Request, Response} from 'express';
 import ClientOAuth2 from 'client-oauth2';
 import axios from 'axios';
-
-const FITBIT_API_BASE_URL = 'https://api.fitbit.com/1/user/-/';
-const FITBIT_API_PROFILE = '/profile.json';
-const FITBIT_API_ACTIVITY_DAILY = '/activities/date/[date].json';
-const FITBIT_API_ACTIVITY_STEP = '/activities/steps/date/today/1d.json';
-const FITBIT_API_ACTIVITY_HEART_RATE = '/activities/heart/date/today/1d.json';
-
-type FitbitUnknownType =
-  | boolean
-  | number
-  | string
-  | FitbitUnknownType[]
-  | {[key: string]: FitbitUnknownType};
-type FitBitClockTimeDisplayFormat = '12hour' | '24hour';
-type FitBitGender = 'FEMALE' | 'MALE' | 'NA';
-type FitBitLocale =
-  | 'en_US'
-  | 'fr_FR'
-  | 'de_DE'
-  | 'es_ES'
-  | 'en_GB'
-  | 'en_AU'
-  | 'en_NZ'
-  | 'ja_JP';
-
-interface FitibitUser {
-  age: number;
-  ambassador: boolean;
-  autoStrideEnabled: boolean;
-  avatar: string;
-  avatar150: string;
-  avatar640: string;
-  averageDailySteps: number;
-  challengesBeta: boolean;
-  clockTimeDisplayFormat: FitBitClockTimeDisplayFormat;
-  corporate: boolean;
-  corporateAdmin: boolean;
-  country: string;
-  dateOfBirth: string;
-  displayName: string;
-  displayNameSetting: string;
-  distanceUnit: string;
-  encodedId: string;
-  features: {
-    exerciseGoal: boolean;
-  };
-  foodsLocale: string;
-  fullName: string;
-  gender: FitBitGender;
-  glucoseUnit: string;
-  height: number;
-  heightUnit: string;
-  isBugReportEnabled: boolean;
-  isChild: boolean;
-  isCoach: boolean;
-  languageLocale: string;
-  legalTermsAcceptRequired: boolean;
-  locale: FitBitLocale;
-  memberSince: string;
-  mfaEnabled: boolean;
-  offsetFromUTCMillis: number;
-  sdkDeveloper: boolean;
-  sleepTracking: string;
-  startDayOfWeek: string;
-  strideLengthRunning: number;
-  strideLengthRunningType: string;
-  strideLengthWalking: number;
-  strideLengthWalkingType: string;
-  swimUnit: string;
-  timezone: string;
-  topBadges: FitibitBadges[];
-  username: string;
-  waterUnit: string;
-  waterUnitName: string;
-  weight: number;
-  weightUnit: string;
-}
-
-export interface FitibitBadges {
-  badgeGradientEndColor: string;
-  badgeGradientStartColor: string;
-  badgeType: string;
-  category: string;
-  cheers: FitbitUnknownType[];
-  dateTime: string;
-  description: string;
-  earnedMessage: string;
-  encodedId: string;
-  image100px: string;
-  image125px: string;
-  image300px: string;
-  image50px: string;
-  image75px: string;
-  marketingDescription: string;
-  mobileDescription: string;
-  name: string;
-  shareImage640px: string;
-  shareText: string;
-  shortDescription: string;
-  shortName: string;
-  timesAchieved: number;
-  unit: string;
-  value: number;
-}
+import {
+  FITBIT_API_ACTIVITY_DAILY,
+  FITBIT_API_ACTIVITY_HEART_RATE,
+  FITBIT_API_ACTIVITY_STEP,
+  FITBIT_API_BASE_URL,
+  FITBIT_API_PROFILE,
+} from '../lib/fitbitApi';
 
 export const fitbitAuth = new ClientOAuth2({
   clientId: process.env.FITBIT_CLIENT_ID,
---
+++ src/lib/fitbitApi.ts
@@ -0,0 +1,208 @@
+export const FITBIT_API_BASE_URL = 'https://api.fitbit.com/1/user/-/';
+export const FITBIT_API_PROFILE = '/profile.json';
+export const FITBIT_API_ACTIVITY_DAILY = '/activities/date/[date].json';
+export const FITBIT_API_ACTIVITY_STEP = '/activities/steps/date/today/1d.json';
+export const FITBIT_API_ACTIVITY_HEART_RATE =
+  '/activities/heart/date/today/1d.json';
+
+type FitbitUnknownType =
+  | boolean
+  | number
+  | string
+  | FitbitUnknownType[]
+  | {[key: string]: FitbitUnknownType};
+
+type FitBitClockTimeDisplayFormat = '12hour' | '24hour';
+
+type FitBitGender = 'FEMALE' | 'MALE' | 'NA';
+
+type FitBitLocale =
+  | 'en_US'
+  | 'fr_FR'
+  | 'de_DE'
+  | 'es_ES'
+  | 'en_GB'
+  | 'en_AU'
+  | 'en_NZ'
+  | 'ja_JP';
+
+interface ResponseFitbitProfile {
+  user: FitibitUser;
+}
+
+interface ResponseFitbitDailyActivitySummary {
+  activities: FitbitActivity;
+  goals: FitbitGoals;
+  summary: FitbitSummary;
+}
+
+interface ResponseFitbitActivityStep {
+  'activities-steps': FitbitStep[];
+  'activities-steps-intraday': FitbitActivitiesIntraday;
+}
+
+interface ResponseFitbitHearrate {
+  'activities-heart': FitbitHeart[];
+  'activities-heart-intraday': FitbitActivitiesIntraday;
+}
+
+interface FitbitDataset {
+  time: string;
+  value: number;
+}
+
+interface FitbitActivitiesIntraday {
+  dataset: FitbitDataset[];
+  datasetInterval: number;
+  datasetType: string;
+}
+
+interface FitibitUser {
+  age: number;
+  ambassador: boolean;
+  autoStrideEnabled: boolean;
+  avatar: string;
+  avatar150: string;
+  avatar640: string;
+  averageDailySteps: number;
+  challengesBeta: boolean;
+  clockTimeDisplayFormat: FitBitClockTimeDisplayFormat;
+  corporate: boolean;
+  corporateAdmin: boolean;
+  country: string;
+  dateOfBirth: string;
+  displayName: string;
+  displayNameSetting: string;
+  distanceUnit: string;
+  encodedId: string;
+  features: {
+    exerciseGoal: boolean;
+  };
+  foodsLocale: string;
+  fullName: string;
+  gender: FitBitGender;
+  glucoseUnit: string;
+  height: number;
+  heightUnit: string;
+  isBugReportEnabled: boolean;
+  isChild: boolean;
+  isCoach: boolean;
+  languageLocale: string;
+  legalTermsAcceptRequired: boolean;
+  locale: FitBitLocale;
+  memberSince: string;
+  mfaEnabled: boolean;
+  offsetFromUTCMillis: number;
+  sdkDeveloper: boolean;
+  sleepTracking: string;
+  startDayOfWeek: string;
+  strideLengthRunning: number;
+  strideLengthRunningType: string;
+  strideLengthWalking: number;
+  strideLengthWalkingType: string;
+  swimUnit: string;
+  timezone: string;
+  topBadges: FitibitBadges[];
+  username: string;
+  waterUnit: string;
+  waterUnitName: string;
+  weight: number;
+  weightUnit: string;
+}
+
+interface FitibitBadges {
+  badgeGradientEndColor: string;
+  badgeGradientStartColor: string;
+  badgeType: string;
+  category: string;
+  cheers: FitbitUnknownType[];
+  dateTime: string;
+  description: string;
+  earnedMessage: string;
+  encodedId: string;
+  image100px: string;
+  image125px: string;
+  image300px: string;
+  image50px: string;
+  image75px: string;
+  marketingDescription: string;
+  mobileDescription: string;
+  name: string;
+  shareImage640px: string;
+  shareText: string;
+  shortDescription: string;
+  shortName: string;
+  timesAchieved: number;
+  unit: string;
+  value: number;
+}
+
+interface FitbitActivity {
+  activityId: number;
+  activityParentId: number;
+  activityParentName: string;
+  calories: number;
+  description: string;
+  duration: number;
+  hasActiveZoneMinutes: boolean;
+  hasStartTime: boolean;
+  isFavorite: boolean;
+  lastModified: string;
+  logId: number;
+  name: string;
+  startDate: string;
+  startTime: string;
+  steps: number;
+}
+
+interface FitbitGoals {
+  activeMinutes: number;
+  caloriesOut: number;
+  distance: number;
+  floors: number;
+  steps: number;
+}
+
+interface FitbitDistance {
+  activity: string;
+  distance: number;
+}
+
+interface FitbitHeartRateZone {
+  caloriesOut: number;
+  max: number;
+  min: number;
+  minutes: number;
+  name: string;
+}
+
+interface FitbitSummary {
+  activeScore: number;
+  activityCalories: number;
+  caloriesBMR: number;
+  caloriesOut: number;
+  distances: FitbitDistance[];
+  elevation: number;
+  fairlyActiveMinutes: number;
+  floors: number;
+  heartRateZones: FitbitHeartRateZone[];
+  lightlyActiveMinutes: number;
+  marginalCalories: number;
+  restingHeartRate: number;
+  sedentaryMinutes: number;
+  steps: number;
+  veryActiveMinutes: number;
+}
+
+interface FitbitStep {
+  dateTime: string;
+  value: string;
+}
+
+interface FitbitHeart {
+  dateTime: string;
+  value: {
+    customHeartRateZones: FitbitHeartRateZone[];
+    heartRateZones: FitbitHeartRateZone[];
+  };
+}
